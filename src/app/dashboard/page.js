6a578092b6f257c800cb7cbfefbdbe124f683ad6
lewyinn <kurniawanridho500@gmail.com>
2026-02-01 00:21:43 +0700

    set min width recharts

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -123,7 +123,7 @@ export default function DashboardPage() {
                             </div>
                         </div>
                         <div className="h-[300px] w-full">
-                            <ResponsiveContainer width="100%" height="100%">
+                            <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={300}>
                                 <AreaChart data={chartData}>
                                     <defs>
                                         <linearGradient id="colorJumlah" x1="0" y1="0" x2="0" y2="1">
